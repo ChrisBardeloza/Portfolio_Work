46ede0f71467cf358adc954b62f6a2a303b7e455
Chris Bardeloza <cbardeloza2@gmail.com>
2020-04-28 13:27:00 -0500

    Basic Knowledge

---
+++ Learning 1.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+# This is for reference to basics
+print('str=String')
+print('int=intager')
+print('float=number with decimal')
+print('bool=true false')
+print('list uses []')
+print('Tuple uses ()')
+print('dict uses {Key:Value}')
